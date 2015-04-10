ea7171595b4e3be123bc5c8786cb01e8075a1feb
Anton Kharenko <a.kharenko@gmail.com>
2015-04-10 11:41:40 +0300

    Added JavaDoc to relation enum

--- src/main/java/com/antonkharenko/cloudclock/Relation.java
+++ src/main/java/com/antonkharenko/cloudclock/Relation.java
@@ -1,14 +1,30 @@
 package com.antonkharenko.cloudclock;
 
 /**
+ * Represents logical relation Happens-Before between two events which happens
+ * in the distributed system.
+ *
+ * <p>Happens-Before relation (denoted below as "->") represented with three
+ * following rules:
+ * <ol>
+ *     <li>On same process: <pre>A -> B, if time(A) < time(B) (using the local clock)</pre></li>
+ *     <li>If process P1 sends message M to process P2: <pre>send(M) -> receive(M)</pre></li>
+ *     <li>Transitivity: <pre>if A -> B and B -> C then A -> C<pre/></li>
+ * </ol>
+ *
  * @author Anton Kharenko
  */
 public enum Relation {
 
-	// TODO: JavaDocs
-
+	/** Timestamps are equal. */
 	EQUAL,
+
+	/** Two events are in causal relation and one event happens before another. */
 	HAPPENS_BEFORE,
+
+	/** Two events are in causal relation and one event happens after another. */
 	HAPPENS_AFTER,
+
+	/** Two events are concurrent and not in causal relation to each other. */
 	CONCURRENT
 }
